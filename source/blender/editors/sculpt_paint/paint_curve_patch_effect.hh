84b280cbd36ad2ccfad39b973466ae61627f3e1a
Nazir Galimov <nazir.galimov@gmail.com>
2026-07-24 15:12:51 +0500

    chore(sculpt_paint): disable curve patch profiling
    set the CURVE_PATCH_PROFILING macro to 0, disabling profiling output for curve patch effects until re-enabled for measurement passes.

--- source/blender/editors/sculpt_paint/paint_curve_patch_effect.hh
+++ source/blender/editors/sculpt_paint/paint_curve_patch_effect.hh
@@ -25,7 +25,7 @@
  * `paint_curve_patch_effect_image.cc` (the `DEBUG-cpatch-image` line). Must stay 0 outside a
  * measurement pass: the reports write to stdout and flush on every interactive re-stamp. Grep
  * `DEBUG-cpatch` for every touch point. */
-#define CURVE_PATCH_PROFILING 1
+#define CURVE_PATCH_PROFILING 0
 
 namespace blender {
 struct Brush;
